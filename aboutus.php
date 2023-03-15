3937945081998b862897fd9366c383cd0c3948cd
charuni perera <119150765+cs2612@users.noreply.github.com>
2023-03-16 05:18:03 +0530

    first commit

---
+++ aboutus.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0 user-scalable=yes">
+    <title>About Us</title>
+   
+<style>
+
+
+{
+    margin: 0;
+    padding: 0;
+    font-family: "Open Sans",sans-serif;
+    box-sizing: border-box;
+}
+ 
+body{
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #f1f1f1;
+}
+ 
+.about-section{
+    background: url("https://i.pinimg.com/736x/a5/92/23/a59223a81638be37d096fcfa72d7dd48.jpg") no-repeat left;
+    background-size: 55%;
+    background-color: #fdfdfd;
+    overflow: hidden;
+    height: 100%;
+    padding: 100px 0;
+}
+ 
+.inner-container{
+    width: 55%;
+    float: right;
+    background-color: #fdfdfd;
+    padding: 150px;
+box-shadow: rgba(14, 30, 37, 0.12) 0px 2px 4px 0px, rgba(14, 30, 37, 0.32) 0px 2px 16px 0px;
+ 
+}
+.inner-container h1{
+    margin-bottom: 30px;
+    font-size: 30px;
+    font-weight: 900;
+}
+ 
+.text{
+    font-size: 13px;
+    color: #545454;
+    line-height: 30px;
+    text-align: justify;
+    margin-bottom: 40px;
+}
+h1{
+  color: #074370;
+}
+ 
+.skills{
+    display: flex;
+    justify-content: space-between;
+    font-weight: 700;
+    font-size: 13px;
+}
+ 
+@media screen and (max-width:320px){
+    .inner-container{
+        padding: 80px;
+    }
+}
+ 
+@media screen and (max-width:320px){
+    .about-section{
+        background-size: 100%;
+        padding: 100px 40px;
+    }
+    .inner-container{
+        width: 100%;
+    }
+}
+ 
+@media screen and (max-width:320px){
+    .about-section{
+        padding: 0;
+    }
+    .inner-container{
+        padding: 60px;
+    }
+}
+
+.button {
+  border: none;
+  color: black;
+  padding: 15px 20px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 17px;
+  margin: 4px 2px;
+  cursor: pointer;
+}
+
+.button {background-color: #7EA6C4;} 
+</style>
+</head>
+<body>
+
+    <div class="about-section">
+        <div class="inner-container">
+            <h1>About Us</h1> <img src="logo.png" height="300px" width="400px" alt="logo" align="right"> 
+            <p class="text">
+            <h3><p><b><i>"Bumble bee"</b></i>is one of the famous electronic company in Sri Lanka, which customers are able to purchase latest electronic items with professional services and Customer can purchase Items 3 interest with free Installments. It is located in  Nugegoda, Borella  and Dehiwala .And it’s the most popular company which provides the latest electronics items with a warranty. They have more than 7 years of history of providing this service. They started their company with PC items and laptops. For 7 years, <b><i>"Bumble bee"</b></i> has met demanding requirements with standards and certain products. After developing during these years they expand their customer base and staff. And it has earned providing the best value in high-quality equipment to the market.</p></h3>
+            </br>
+            
+               <h1><b>Company Mission.....</b></h1>    
+		   <p><b>"Being one of the fastest growing E-commerce platforms and  provide  the most latest  technology and technical support from our professional staff, in addition to high-quality products, quality parts, and service"</b></p>
+		   </br>
+	For more details visit our web page
+        <div class="right"></br>
+		<b><a href="http://google.com"title="To get more details visit here" target="_blank">Bumble bee Electronic Store</a></b>
+			</br>
+</br>
+<button class="button"><a href="home.php" target="_self" class ="nav-link"><b>Back to Home Page </b></a></button>
+</br>
+</br>
+</br>
+<center><p class="footer-company-name">Bumblebee &copy; 2023</p></center>
+	</div>
+        </div>
+    </div>
+</body>
+</html>
