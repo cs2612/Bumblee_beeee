e79c52ccf45e864413872f4b1e159e676917fbe3
charuni perera <119150765+cs2612@users.noreply.github.com>
2023-03-17 03:23:51 +0530

    new upload

---
+++ payment gateway/placeorder.php
@@ -0,0 +1,6 @@
+<html>
+<head>
+    <h1><b><i>Your Order Has Been Placed</b><i></h1>
+    <p>Thank you for ordering with us, we'll contact you by email with your order details.</p>
+</head>
+</html>
