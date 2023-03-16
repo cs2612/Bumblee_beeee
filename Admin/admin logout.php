0d8a86911e5df0e65c327bfd48b327bce4a663e4
charuni perera <119150765+cs2612@users.noreply.github.com>
2023-03-17 03:27:10 +0530

    Delete Admin directory

--- Admin/admin logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-session_start();
-
-session_destroy();
-
-echo "<script>window.open('admin login.php','_self')</script>";
-
-?>
--- Admin/includes/db.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$con = mysqli_connect("localhost","root","","bumblee_bee1");
-
-?>
