5a14024bbc3e52a240531e4b5c2a800156bc95b2
charuni perera <119150765+cs2612@users.noreply.github.com>
2023-03-20 05:01:52 +0530

    new upload

---
+++ home.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="style.css" />
+  <meta name="keywords" content="footer, address, phone, icons" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
+<link href='https://fonts.googleapis.com/css?family=Raleway:600,900,400' rel='stylesheet' type='text/css'>
+<link href="http://fonts.googleapis.com/css?family=Cookie" rel="stylesheet" type="text/css">
+<title>Bumble bee | Home</title>
+</head>
+
+<body>
+<nav class="navbar">
+<h1 class="logo">B<span>b</span></h1>
+<ul class="nav-links">
+<input type="checkbox" id="checkbox_toggle" />
+<label for="checkbox_toggle" class="hamburger">&#9776;</label>
+<div class="menu">
+<li class="home">
+<a class="active" href="home.php" target="_self"><i  class="fa fa-fw fa-home"></i>Home<i class="fa fa-caret-down"></i></a>
+<ul class="dropdown">
+<li><a href="http://localhost/bumblebee/aboutus.php">About Us</a></li>
+</ul>
+</li>
+
+<li class="products">
+<a href="product.php"><i class="fa fa-shopping-basket"></i>Products<i class="fa fa-caret-down"></i></a>
+<ul class="dropdown">
+<li><a href="http://localhost/bumblebee/category1.php">Laptops</a></li>
+<li><a href="http://localhost/bumblebee/category2.php">Desktops</a></li>
+<li><a href="http://localhost/bumblebee/category3.php">Smart Phones</a></li>
+<li><a href="http://localhost/bumblebee/category4.php">Smart watches</a></li>
+<li><a href="http://localhost/bumblebee/category5.php">Cameras</a></li>
+<li><a href="http://localhost/bumblebee/category6.php">Tv Items</a></li>
+<li><a href="http://localhost/bumblebee/category7.php">Other Items</a></li>
+</ul>
+</li>
+
+<li><a href="contact.php" target="_self"  class ="nav-link"  ><i class="fa fa-phone"></i>Contact</a></li>
+				
+<li><a href="register.php"   target="_self" class ="nav-link" ><i class="fa fa-pencil"></i>Register</a></li>
+				
+<li><a href="signin.php"   target="_self" class ="nav-link" ><i class="fa fa-fw fa-user"></i>Signin</a></li>
+				
+<li><a href="addtocart.php" target="_self" class ="nav-link" ><i class="fa fa-fw fa-shopping-cart"></i>Cart</a></li>
+
+<a class="nav-link" href="logout.php"><i class="fa fa-sign-out"></i>Logout</a>
+
+<form class="Oflox-Search-box" role="search" method="get"
+action="http://localhost/bumblebee/category1.php"><input type="search" id="oflox-block-
+search__input-3" class="Oflox-Search-box" name="s" value="" 
+placeholder="Search Here..." style="width: 150px; height: 22px; align: 'flex';"><button type="submit" class="fa fa-fw fa-search" style="width: 22px; height: 22px;"></i></button></form>
+
+</div>	
+</ul>
+</nav>
+<div class="cookies-eu-banner hidden">
+  <style>
+   .cookies-eu-banner {
+  background: #8fbc8f ;
+  color: #fff;
+  padding: 6px;
+  font-size: 16px;
+  text-align: center;
+}
+.cookies-eu-banner button {
+  text-decoration: none;
+  background: #24bb51;
+  color: black;
+  border: 1px solid #000;
+  cursor: pointer;
+  margin: 2px 0;
+  font-size: 15px;
+  }
+  .blacktext{
+    color: black;
+  }
+</style>
+  <h1 class="blacktext"><b><i>Allow Cookies</i></b></h1>
+     This website uses cookies to ensure you get the best experience on our website.<a href="http://localhost/bumblebee/home.php">Privacy&Policy</a>
+      <button>Decline</button>
+      <button>Accept</button>
+      <script src="main.js"></script>
+    </div>
+  
+<div class="hero">
+      <div class="container">
+        <h1><b>Welcome To Bumble bee <span><i>Electronic Store</i><span></b></h1>
+        <p>Do you like to buy high-quality products from us and <b>pay later</b>?</p>
+      </div>
+    </div>
+
+    <footer class="footer-distributed">
+ 
+ <div class="footer-left">
+
+   <h3>Bumble<span>bee</span></h3>
+
+   <p class="footer-links">
+     <a href="index.php">Home</a>
+     ·
+     <a href="product.php">Products</a>
+     ·
+     <a href="contact.php">Contact</a>
+     ·
+     <a href="register.php">Register</a>
+     ·
+     <a href="signin.php">Sign in</a>
+     ·
+     <a href="viewcart.php">Cart</a>
+   </p>
+
+   <p class="footer-company-name">Bumblebee &copy; 2023</p>
+ </div>
+
+ <div class="footer-center">
+
+   <div>
+     <i class="fa fa-map-marker"></i>
+     <p><span>Nugegoda , Dehiwala , Borella </span>SL(Sri Lanka)</p>
+   </div>
+
+   <div>
+     <i class="fa fa-phone"></i>
+     <p>+94 110983567</P>  
+                         <P>+94 111993568</p>
+   </div>
+
+   <div>
+     <i class="fa fa-envelope"></i>
+     <p><a href="mailto:support@company.com">bumblebee@gmail.com
+</a></p>
+   </div>
+
+ </div>
+
+ <div class="footer-right">
+
+   <p class="footer-company-about">
+     <span>About the company</span>
+     Bumble bee company is a high-quality guranteed electronic items selling company.
+   </p>
+
+   <div class="footer-icons">
+
+     <a href="#"><i class="fa fa-facebook"></i></a>
+     <a href="#"><i class="fa fa-twitter"></i></a>
+     <a href="#"><i class="fa fa-linkedin"></i></a>
+     <a href="#"><i class="fa fa-instagram"></i></a>
+     <a href="#"><i class="fa fa-whatsapp"></i></a>
+                      
+   </div>
+
+ </div>
+
+  </footer>
+</section>
+</body>
+</html>
