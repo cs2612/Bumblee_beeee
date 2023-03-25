76b5c2d891521a0b58a47c2f849085ec0f37227c
charuni perera <119150765+cs2612@users.noreply.github.com>
2023-03-25 05:46:36 +0530

    new upload

---
+++ payment gateway/placeorder.php
@@ -0,0 +1,7 @@
+<html>
+<head>
+    <h1><b><i>Your Order Has Been Placed</b><i></h1>
+    <p>Thank you for ordering with us, we'll contact you by email with your order details.</p>
+    <a href="http://localhost/bumblebee/home.php"  input type="submit" class="submit-btn" >Back to Shopping</a>
+</head>
+</html>
