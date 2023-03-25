072e5a94463f46ddecec05dad98f6fcb340ec4ec
charuni perera <119150765+cs2612@users.noreply.github.com>
2023-03-25 05:45:42 +0530

    new upload

---
+++ payment gateway/installment.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+    
+.container {
+  position: relative;
+  text-align: center;
+  color: rgb(9, 9, 9);
+  background-color: antiquewhite;
+  width: 100%;
+  height: 500px;
+}
+
+
+.centered {
+  position: absolute;
+  top: 20%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+</style>
+<title>Bumble bee| Installment Payment</title>
+</head>
+<body>
+    <h1 style=" text-align: center;
+    color: rgb(88, 11, 11);">Payment Plan</h1>
+<div class="container">
+<p><h2>If your product price is up to 15,000 LKR you can paid it as a loan basically in 3 interest-free installments.</h2></p>
+<p>Ex-: If you product price is 12,000 LKR</p>
+<p>You can pay with it :</p>
+<p> <b>In First Installment</b> -:  4,000 LKR x 0 interest</p>
+<p> <b>In Second Installment</b> -: 4,000 LKR x 0 interest</p>
+<p> <b>In Third Installment</b> -:  4,000 LKR x 0 interest</p>
+<p>Your bank is paying us 12,000 LKR. So, You have to pay 4,000 per month without any interest to your bank.<p>
+<button class="button"><a href="index.php" target="_self" class ="nav-link" style="align:center;"><b>Back to Payment Page </b></a></button>
+<img src="images\installment.png" alt="intallment" width="350" height="350">
+
+    </div>
+    <footer>
+        <div class ="footer">
+		      <p class="footer-company-name"><b>Bumblebee &copy; 2023</b></p>
+			 </div>
+			 </footer>
+    </body>
+   
+    </html>
